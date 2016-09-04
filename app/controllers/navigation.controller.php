2d49263ed60cc4ece5aae0bcce96d39d2a0959c5
Fernandogza <fernandogarzaconde@gmail.com>
2016-09-04 11:55:09 -0500

    About us pages.

---
+++ app/controllers/navigation.controller.php
@@ -0,0 +1,15 @@
+<?php
+
+$app->get("/sobre_nosotros/acerca", function () use ($app) {
+  $app->render('acerca_nosotros.html.twig');
+});
+
+$app->get("/sobre_nosotros/derechos_humanos", function () use ($app) {
+  $app->render('derechos_humanos.html.twig');
+});
+
+$app->get("/sobre_nosotros/contacto", function () use ($app) {
+  $app->render('ubicacion_contacto.html.twig');
+});
+
+?>
